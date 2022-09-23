4e73b6761dd10ca521ec45837b203baea04f5305
Anjali Patel <pa4904550@gmail.com>
2022-09-22 22:52:00 -0400

    Fixed comments to follow style guide, fixed return statement

--- A1Code.py
+++ A1Code.py
@@ -8,22 +8,20 @@ db = SQLAlchemy(app)
 
 class Review(db.model):
   id = db.Column(db.Integer, primary_key=True)
-  """ setting up Review data model to allow verified
-  guest to the listing to leave a review of their stay at the property."""
+  # setting up Review data model to allow verified
+  # guest to the listing to leave a review of their stay at the property.
   rating = db.Column(db.Integer, primary_key=True, nullable=False)
   # Leaving an integer rating, ie 5/5.
   commentTitle = db.Column(db.string(120), nullable=False) 	
-  """Short title regarding the review saved as string, ie Glass sauna was perfect for
-  warming up after swim """
+  # Short title regarding the review saved as string, ie Glass sauna was 
+  # perfect for warming up after swim 
   comment = db.Column(db.string(2000)) 
-  """Detailed review of the stay that a customer expereinced saved as string"""
+  # Detailed review of the stay that a customer expereinced saved as string
   dateReviewed = db.Column(db.string(30), nullable=False)
   # Date the review was left, saved as string
   usernameReviewer = db.Column(db.String(80), nullable=False)
   # Username of the person that is leaving the review
 
-# camel case only , no snake case
   def __repr__(self):
-      return '<review id%r>' % self.id
-      #Who, date of the review posted, what the actual revies
-    
+      return '<Review ID %r>' % self.id
+   
