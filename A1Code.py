ec084da6677ff09dff3681e663d652c191c5ff47
Anjali Patel <pa4904550@gmail.com>
2022-09-23 13:29:46 -0400

    Edited the comments, spacings, and blank lines

--- A1Code.py
+++ A1Code.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-# setting up SQLAlchemy and data models so we can map data models into database tables
+# setting up SQLAlchemy and data models so we can map data models
+# into database tables
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-db = SQLAlchemy(app) 
+db = SQLAlchemy(app)
+
 
 class Review(db.model):
     # Setting up Review data model to allow verified
@@ -12,17 +14,16 @@ class Review(db.model):
     id = db.Column(db.Integer, primary_key=True)
     # Leaving an integer rating, i.e. 5/5.
     rating = db.Column(db.Integer, primary_key=True, nullable=False)
-    # Short title regarding the review saved as string, ie Glass sauna was 
+    # Short title regarding the review saved as string, ie Glass sauna was
     # perfect for warming up after swim
-    commentTitle = db.Column(db.string(120), nullable=False) 	 
+    commentTitle = db.Column(db.string(120), nullable=False)
     # Detailed review of the stay that a customer expereinced saved as string
-    comment = db.Column(db.string(2000)) 
+    comment = db.Column(db.string(2000))
     # Date the review was left, saved as string
     dateReviewed = db.Column(db.string(30), nullable=False)
     # Username of the person that is leaving the review
     usernameReviewer = db.Column(db.String(80), nullable=False)
-  
+
 
 def __repr__(self):
     return '<Review ID %r>' % self.id
-   
