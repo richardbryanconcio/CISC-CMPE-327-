1cd64a8e3397fed02da9277d3b2956ebda108710
Anjali Patel <pa4904550@gmail.com>
2022-09-22 23:04:02 -0400

    Moved comments before the commands and edited the comments

--- A1Code.py
+++ A1Code.py
@@ -7,20 +7,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 db = SQLAlchemy(app) 
 
 class Review(db.model):
-  id = db.Column(db.Integer, primary_key=True)
-  # setting up Review data model to allow verified
+  # Setting up Review data model to allow verified
   # guest to the listing to leave a review of their stay at the property.
+  id = db.Column(db.Integer, primary_key=True)
+  # Leaving an integer rating, i.e. 5/5.
   rating = db.Column(db.Integer, primary_key=True, nullable=False)
-  # Leaving an integer rating, ie 5/5.
-  commentTitle = db.Column(db.string(120), nullable=False) 	
   # Short title regarding the review saved as string, ie Glass sauna was 
-  # perfect for warming up after swim 
-  comment = db.Column(db.string(2000)) 
+  # perfect for warming up after swim
+  commentTitle = db.Column(db.string(120), nullable=False) 	 
   # Detailed review of the stay that a customer expereinced saved as string
-  dateReviewed = db.Column(db.string(30), nullable=False)
+  comment = db.Column(db.string(2000)) 
   # Date the review was left, saved as string
-  usernameReviewer = db.Column(db.String(80), nullable=False)
+  dateReviewed = db.Column(db.string(30), nullable=False)
   # Username of the person that is leaving the review
+  usernameReviewer = db.Column(db.String(80), nullable=False)
+  
 
   def __repr__(self):
       return '<Review ID %r>' % self.id
