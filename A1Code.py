4df8b786dfc937cbd903ef693024fa5234cf5df1
richardbryanconcio <richardconcio9@gmail.com>
2022-09-22 21:30:46 -0400

    changed parameters for money
    updated the parameters

--- A1Code.py
+++ A1Code.py
@@ -29,7 +29,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
 
     # added columns for user data model
-    money = db.Column(db.float, primary_key=True)
+    money = db.Column(db.float, unique=True, nullable=False)
     password = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
