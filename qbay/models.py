ecadb0cd48b8b5f139977cc06be2a72f9de8ecdc
richardbryanconcio <richardconcio9@gmail.com>
2022-10-10 15:50:48 -0400

    Login and Verification Functions
    -Added login function
    -Added email verification function

--- qbay/models.py
+++ qbay/models.py
@@ -107,10 +107,40 @@ def login(email, password):
       Returns:
         The user object if login succeeded otherwise None
     '''
-    valids = User.query.filter_by(email=email, password=password).all()
-    if len(valids) != 1:
+    # Case for when length of email is false
+    if len(email) == 0:
         return None
-    return valids[0]
+        print("Email is empty")
+
+    # Case for when length of password is false
+    if len(password) == 0:
+        return None
+        print("Password is empty")
+
+    # Tests if it has an uppercase, lowercase, contains a digit, or if string length is above 6
+    if (any(x.isupper() for x in password) and any(x.islower() for x in password)
+        and any(x.isdigit() for x in password) and len(password) >= 6) is False:
+        return None
+
+    # Validating the data of emails and passwords from the database
+    validatedAccounts = User.query.filter_by(email=email, password=password.all())
+    if len(validatedAccounts) != 1:
+        return None
+    return validatedAccounts[0]
+
+
+    # Includes an 'import re' at the top
+    # This gives all the characters that are checked within email string
+    # Checks whether the email entered is a valid email or not using the regex below
+def emailValidation(email):
+    regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+    if re.fullmatch(regex, email):
+        print("This is a valid email")
+        return True
+    else:
+        print("This is an invalid email")
+        return False
+   
 
 def update(field, new):
     '''
