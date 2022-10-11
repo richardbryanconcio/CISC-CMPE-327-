13670d6a4809fe97b76988f7684334daf068c563
Anton Gudonis <anton@gudonis.com>
2022-10-10 21:04:36 -0400

    created create listing function

--- qbay/models.py
+++ qbay/models.py
@@ -1,6 +1,7 @@
 from qbay import app
 from flask_sqlalchemy import SQLAlchemy
 import re
+from datetime import date
 
 '''
 This file defines data models and related business logics
@@ -14,8 +15,8 @@ db = SQLAlchemy(app)
 # of listings for the day.
 class Listing(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, unique = True, nullable = False)
-    description = db.Column(db.String)
+    title = db.Column(db.String(80), unique = True, nullable = False)
+    description = db.Column(db.String(2000))
     price = db.Column(db.Float, nullable = False)
     lastModifiedDate = db.Column(db.Date, nullable = False)
     ownerId = db.Column(db.Integer, nullable = False)
@@ -121,6 +122,74 @@ def update(field, new):
       Returns:
         True if the change went through false if operation failed
     '''
+def createListing(title, description, price, user, startDate, endDate):
+    '''
+    Create a new listing
+      Parameters:
+        title (string): title of the listing, must be no longer then 80 characters 
+        description (string): description of the listings, 
+        must be longer then title, more then 20 characters and no longer then 20000 characters
+        price (float): price of the listing bounded to [10, 10000]
+        user (int): userId of the user who created the listing
+        startDate (date): starting date of avalibilty for the listing
+        endDate (date): last day of avalibility for the listing
+      Returns:
+        Returns the listingId if succussful, None otherwise
+    '''
+    # check if title has been used
+    existed = Listing.query.filter(title=title).all()
+    if len(existed) > 0:
+        return None
+    
+    # check if title has leading or trailing white space
+    # option avalible trim title in the future instead of not creating the listing
+    if title[0] == ' ' or title[len(title) - 1] == ' ':
+        return None
+
+    # check if title is alphanumeric excluding spaces 
+    tempTitle = title.replace(' ', '')
+    if  not tempTitle.isalnum():
+        return None
+
+    # check if title is proper length
+    if len(title) > 80 or title == '':
+        return None
+    
+    # check if description is proper length
+    if len(description) > 2000 or len(description) < 20:
+        return None
+
+    # check if description is shorter then title
+    if len(description) < len(title):
+        return None
+
+    # check if price is within proper range
+    if price < 10 or price > 10000:
+        return None
+
+    # check if end date is after start date 
+    if endDate < startDate:
+        return None
+
+    # if all checks pass create the listing 
+    listing = Listing(title=title, description=description, price=price, lastModfiedDate=date.today(), ownerId=user.Id, startDate=startDate, endDate=endDate)  
+
+    db.session.add(listing)
+
+    db.session.commit()
+
+def updateListing(field, new, title, user):
+    '''
+    Update an existing listing
+      Parameters:
+        field (String): field to be changed
+        new (string): value to replace field, to be typecasted if field to be changed is not a string
+        title (string): title of the listing to be changed
+        user (db.model): user to confirm user is the owner of the listing
+      Returns:
+        True if the change went through false if operation failed
+    '''
+    pass
 
 def checkpass(password):
     regexpass = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[^\W_]{6,}$"
