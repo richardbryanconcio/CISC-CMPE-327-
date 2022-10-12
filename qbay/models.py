8c3d60c0ba84a43775eff4d52b7aaa05d175f403
Malvirex <97366352+Malvirex@users.noreply.github.com>
2022-10-12 16:46:30 -0400

    Create and update listings (#16)
    * added test cases for create listings function
    
    * created create listing function
    
    * added inital value to register function to remove nullable error
    
    * temporarily removing uniqueness tags from user db model to attempt to be able to do tests
    
    * create listing function works, has passed all tests
    
    * added test cases for update listing
    
    * updateListing function now working, updated some test cases too
    
    * added to confirm updating listings does indeed update the listing

--- qbay/models.py
+++ qbay/models.py
@@ -1,6 +1,7 @@
 from qbay import app
 from flask_sqlalchemy import SQLAlchemy
 import re
+from datetime import date
 
 '''
 This file defines data models and related business logics
@@ -14,8 +15,8 @@ db = SQLAlchemy(app)
 # of listings for the day.
 class Listing(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, unique = True, nullable = False)
-    description = db.Column(db.String)
+    title = db.Column(db.String(80), unique = True, nullable = False)
+    description = db.Column(db.String(2000))
     price = db.Column(db.Float, nullable = False)
     lastModifiedDate = db.Column(db.Date, nullable = False)
     ownerId = db.Column(db.Integer, nullable = False)
@@ -44,12 +45,12 @@ class Booking(db.Model):
 # Therefore, the model has access to id, username, and email databases
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
     password = db.Column(db.String(120), nullable=False)
     billingAddress = db.Column(db.String)
     postalCode = db.Column(db.String)
-    balance = db.Column(db.Float, nullable = False)
+    balance = db.Column(db.Float, nullable=False)
 
 
     def __repr__(self):
@@ -89,7 +90,7 @@ def register(name, email, password):
         return False
 
     # create a new user
-    user = User(username=name, email=email, password=password)
+    user = User(username=name, email=email, password=password, balance=100)
     # add it to the current database session
     db.session.add(user)
     # actually save the user object
@@ -121,6 +122,138 @@ def update(field, new):
       Returns:
         True if the change went through false if operation failed
     '''
+def createListing(title, description, price, user, startDate, endDate):
+    '''
+    Create a new listing
+      Parameters:
+        title (string): title of the listing, must be no longer then 80 characters 
+        description (string): description of the listings, 
+        must be longer then title, more then 20 characters and no longer then 20000 characters
+        price (float): price of the listing bounded to [10, 10000]
+        user (int): userId of the user who created the listing
+        startDate (date): starting date of avalibilty for the listing
+        endDate (date): last day of avalibility for the listing
+      Returns:
+        Returns the listing if succussful, None otherwise
+    '''
+    # check if title has been used
+    existed = Listing.query.filter_by(title=title).all()
+    if len(existed) > 0:
+        return None
+    
+    # check if title has leading or trailing white space
+    # option avalible trim title in the future instead of not creating the listing
+    if title[0] == ' ' or title[len(title) - 1] == ' ':
+        return None
+
+    # check if title is alphanumeric excluding spaces 
+    tempTitle = title.replace(' ', '')
+    if  not tempTitle.isalnum():
+        return None
+
+    # check if title is proper length
+    if len(title) > 80 or title == '':
+        return None
+    
+    # check if description is proper length
+    if len(description) > 2000 or len(description) < 20:
+        return None
+
+    # check if description is shorter then title
+    if len(description) < len(title):
+        return None
+
+    # check if price is within proper range
+    if price < 10 or price > 10000:
+        return None
+
+    # check if end date is after start date 
+    if endDate < startDate:
+        return None
+
+    # if all checks pass create the listing 
+    listing = Listing(title=title, description=description, price=price, lastModifiedDate=date.today(), ownerId=user.id, startDate=startDate, endDate=endDate)  
+
+    db.session.add(listing)
+
+    db.session.commit()
+
+    return listing
+
+def updateListing(field, new, listing, user):
+    '''
+    Update an existing listing
+      Parameters:
+        field (String): field to be changed
+        new (any): value to replace field
+        listing (db.model): the listing object to be changed
+        user (db.model): user to confirm user is the owner of the listing
+      Returns:
+        True if the change went through false if operation failed
+    '''
+    # if listing was not created by the user then don't let them modify the listing
+    if listing.ownerId != user.id:
+        return False
+    # want to put a match-case statement here but ide is flagging me, will try to include later
+    if field == 'title':
+        # new title can not have leading or trailing white spaces
+        if new[0] == ' ' or new[len(new) - 1] == ' ':
+            return False
+        # new title can not be the same as any other titles 
+        existed = Listing.query.filter_by(title=new).all()
+        if len(existed) > 0:
+            return False
+        # new title must be alphanumeric (excluding spaces)
+        tempTitle = new.replace(' ', '')
+        if  not tempTitle.isalnum():
+            return False
+        # new title must be less then 80 characters and not empty
+        if len(new) > 80 or new == '':
+            return False 
+        # length of title can not be longer then description 
+        if len(new) > len(listing.description):
+            return False
+
+        listing.title = new
+        listing.lastModifiedDate = date.today()
+        db.session.commit()
+        return True
+    elif field == 'description':
+        # new description must be between 20-2000 characters, must contain more characters then the title
+        if len(new) < 20 or len(new) > 2000 or len(new) < len(listing.title):
+            return False
+        
+        listing.description = new
+        listing.lastModifiedDate = date.today()
+        db.session.commit()
+        return True
+    elif field == 'price':
+        # price can not be reduced, price can not be above 10000
+        if new > 10000 or new < listing.price:
+            return False
+        
+        listing.price = new
+        listing.lastModifiedDate = date.today()
+        db.session.commit()
+        return True
+    elif field == 'startDate':
+        if new < date.today() or new > listing.endDate:
+            return False
+        
+        listing.startDate = new
+        return True
+    elif field == 'endDate':
+        if new < listing.startDate:
+            return False
+        
+        listing.endDate = new
+        listing.lastModifiedDate = date.today()
+        db.session.commit()
+        return True
+    else:
+        return False
+
+    
 
 def checkpass(password):
     regexpass = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[^\W_]{6,}$"
