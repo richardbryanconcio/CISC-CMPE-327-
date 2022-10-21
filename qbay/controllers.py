0b002efb67e51eda15b8af8822e905a40f48662b
Anton Gudonis <anton@gudonis.com>
2022-10-20 21:13:44 -0400

    added listing view in home with clickable links on listings to only show listing

--- qbay/controllers.py
+++ qbay/controllers.py
@@ -1,10 +1,22 @@
 from flask import render_template, request, session, redirect
 from qbay.models import login, register, update, User, createListing, updateListing, Listing
 
+from datetime import date
+
 from qbay import app 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def home_get():
     # templates are stored in the templates folder
-    return render_template('home.html', message='')
+    products = Listing.query.all()
+    
+    return render_template('home.html', products=products)
+
+
+@app.route('/listing<listingId>')
+def listing_get(listingId):
+    
+    products = Listing.query.filter_by(id=listingId)
+
+    return render_template('home.html', products=products)
 
