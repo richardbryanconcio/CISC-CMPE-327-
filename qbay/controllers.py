76a833e1b6f0177fe211f2944ce3c56c47666d7e
Anjali Patel <pa4904550@gmail.com>
2022-10-24 22:14:51 -0400

    Fixed pep8 for line 2 in controllers

--- qbay/controllers.py
+++ qbay/controllers.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, session, redirect
-from qbay.models import login, register, update, User, createListing, updateListing, Listing
+from qbay.models import (login, register, update, User, 
+                         createListing, updateListing, Listing)
 
 
 from qbay import app
