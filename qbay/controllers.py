d215271bcd266312db3c857a1fb6c7c9920b1274
Anjali Patel <pa4904550@gmail.com>
2022-10-28 14:40:11 -0400

    Removed parts that i was not responsible for

--- qbay/controllers.py
+++ qbay/controllers.py
@@ -6,57 +6,6 @@ from qbay.models import (login, register, update, User,
 from qbay import app
 
 
-def authenticate(inner_function):
-    """
-    :param inner_function: any python function that accepts a user object
-    Wrap any python function and check the current session to see if 
-    the user has logged in. If login, it will call the inner_function
-    with the logged in user object.
-    To wrap a function, we can put a decoration on that function.
-    Example:
-    @authenticate
-    def home_page(user):
-        pass
-    """
-
-    def wrapped_inner():
-
-        # check did we store the key in the session
-        if 'logged_in' in session:
-            email = session['logged_in']
-            try:
-                user = User.query.filter_by(email=email).one_or_none()
-                if user:
-                    # if the user exists, call the inner_function
-                    # with user as parameter
-                    return inner_function(user)
-            except Exception:
-                pass
-        else:
-            # else, redirect to the login page
-            return redirect('/login')
-
-    # return the wrapped version of the inner_function:
-    return wrapped_inner
-
-
-@app.route('/')
-@authenticate
-def home(user):
-    # authentication is done in the wrapper function
-    # see above.
-    # by using @authenticate, we don't need to re-write
-    # the login checking code all the time for other
-    # front-end portals
-
-    # some fake product data
-    products = [
-        {'name': 'prodcut 1', 'price': 10},
-        {'name': 'prodcut 2', 'price': 20}
-    ]
-    return render_template('index.html', user=user, products=products)
-
-
 @app.route('/register', methods=['GET'])
 def register_get():
     # templates are stored in the templates folder
@@ -88,9 +37,3 @@ def register_post():
     else:
         return redirect('/login')
 
-
-@app.route('/logout')
-def logout():
-    if 'logged_in' in session:
-        session.pop('logged_in', None)
-    return redirect('/')
