1d5205e4013315cf679df31c78336b023ab62c3e
BigSchneids5 <isaac.schneider41@gmail.com>
2022-11-11 12:00:25 -0500

    Add update integration test template

---
+++ qbay_test/frontEnd/test_userUpdate.py
@@ -0,0 +1,41 @@
+from seleniumBase import Basecase
+
+import org.openqa.selenium.By as By
+
+from qbay_test.conftest import base_url
+from unittest.mock import patch
+from qbay.models import User
+
+# Contains integration tests for the user update page
+
+class FrontEndUserUpdatePageTest(Basecase):
+
+    #TEST CASE 1: UPDATE USERNAME SUCCESS
+    def test_update_username_success(self, *_):
+
+    #TEST CASE 2: UPDATE USERNAME FAILURE
+    def test_update_username_failure(self, *_):
+
+    #TEST CASE 3: UPDATE PASSWORD SUCCESS
+    def test_update_password_success(self, *_):
+
+    #TEST CASE 4: UPDATE PASSWORD FAILURE
+    def test_update_password_failure(self, *_):
+
+    #TEST CASE 5: UPDATE EMAIL SUCCESS
+    def test_update_email_success(self, *_):
+
+    #TEST CASE 6: UPDATE EMAIL FAILURE
+    def test_update_email_failure(self, *_):
+
+    #TEST CASE 7: UPDATE BILLING ADDRESS SUCCESS
+    def test_update_address_success(self, *_):
+
+    #TEST CASE 8: UPDATE BILLING ADDRESS FAILURE
+    def test_update_address_failure(self, *_):
+
+    #TEST CASE 9: UPDATE POSTAL CODE SUCCESS
+    def test_update_postal_success(self, *_):
+
+    #TEST CASE 10: UPDATE POSTAL CODE FAILURE
+    def test_update_postal_failure(self, *_):
