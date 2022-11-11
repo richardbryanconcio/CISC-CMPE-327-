15fb179a0629bc6ff507b389b447e33a6f728b7b
BigSchneids5 <isaac.schneider41@gmail.com>
2022-11-11 15:20:21 -0500

    Branch name change

--- qbay_test/frontEnd/test_userUpdate.py
+++ qbay_test/frontEnd/test_userUpdate.py
@@ -19,60 +19,83 @@ class FrontEndUserUpdatePageTest(Basecase):
 
     # TEST CASE 1: UPDATE USERNAME SUCCESS
     def test_update_username_success(self, *_):
-        self.open(base_url + '/updateUsername')
+        self.open(base_url + 'baseUpdate/updateUsername')
+
+        # check that current username is displayed
+        self.assert_text(user.username, "currentUsername")
+
         self.type("#username", "newname")
         self.click('input[type="submit"]')
 
-    # TEST CASE 2: UPDATE USERNAME FAILURE
+    # TEST CASE 2: UPDATE USERNAME FAILURE (invalid name)
     def test_update_username_failure(self, *_):
-        self.open(base_url + '/updateUsername')
+        self.open(base_url + 'baseUpdate/updateUsername')
+
+        # check that current username is displayed
+        self.assert_text(user.username, "currentUsername")
+
         self.type("#username", " invalidname")
         self.click('input[type="submit"]')
 
-    # TEST CASE 3: UPDATE PASSWORD SUCCESS
-    def test_update_password_success(self, *_):
-        self.open(base_url + '/updatePassword')
-        self.type("#password", "Abracadabra123$")
+        self.assert_title(base_url)
+
+    # TEST CASE 3: UPDATE USERNAME FAILURE (empty string)
+    def test_update_username_failure(self, *_):
+        self.open(base_url + 'baseUpdate/updateUsername')
+
+        # check that current username is displayed
+        self.assert_text(user.username, "currentUsername")
+
+        self.type("#username", "")
         self.click('input[type="submit"]')
 
-    # TEST CASE 4: UPDATE PASSWORD FAILURE
+        self.assert_title(base_url)
+
+    # TEST CASE X: UPDATE PASSWORD SUCCESS
+    def test_update_password_success(self, *_):
+        self.open(base_url + 'baseUpdate/updatePassword')
+        self.type("#password", "Abracadabra123$")
+        self.type("#confPassword", "Abracadabra123$")
+        self.click('input[type="submit"]')
+
+    # TEST CASE X: UPDATE PASSWORD FAILURE
     def test_update_password_failure(self, *_):
-        self.open(base_url + '/updatePassword')
+        self.open(base_url + 'baseUpdate/updatePassword')
         self.type("#password", "abc")
         self.click('input[type="submit"]')
 
-    # TEST CASE 5: UPDATE EMAIL SUCCESS
+    # TEST CASE X: UPDATE EMAIL SUCCESS
     def test_update_email_success(self, *_):
-        self.open(base_url + '/updateEmail')
+        self.open(base_url + 'baseUpdate/updateEmail')
         self.type("#email", "ricky@trailerpark.com")
         self.click('input[type="submit"]')
 
-    # TEST CASE 6: UPDATE EMAIL FAILURE
+    # TEST CASE X: UPDATE EMAIL FAILURE
     def test_update_email_failure(self, *_):
-        self.open(base_url + '/updateEmail')
+        self.open(base_url + 'baseUpdate/updateEmail')
         self.type("#email", "notanemail.com")
         self.click('input[type="submit"]')
 
-    # TEST CASE 7: UPDATE BILLING ADDRESS SUCCESS
+    # TEST CASE X: UPDATE BILLING ADDRESS SUCCESS
     def test_update_address_success(self, *_):
-        self.open(base_url + '/updateBillingPostal')
+        self.open(base_url + 'baseUpdate/updateBillingPostal')
         self.type("#address", "2525 Maple Ln")
         self.click('input[type="submit"]')
 
-    # TEST CASE 8: UPDATE BILLING ADDRESS FAILURE
+    # TEST CASE X: UPDATE BILLING ADDRESS FAILURE
     def test_update_address_failure(self, *_):
-        self.open(base_url + '/updateBillingPostal')
+        self.open(base_url + 'baseUpdate/updateBillingPostal')
         self.type("#address", "ur moms house")
         self.click('input[type="submit"]')
 
-    # TEST CASE 9: UPDATE POSTAL CODE SUCCESS
+    # TEST CASE X: UPDATE POSTAL CODE SUCCESS
     def test_update_postal_success(self, *_):
-        self.open(base_url + '/updateBillingPostal')
+        self.open(base_url + 'baseUpdate/updateBillingPostal')
         self.type("#postalCode", "b7g 4k5")
         self.click('input[type="submit"]')
 
-    # TEST CASE 10: UPDATE POSTAL CODE FAILURE
+    # TEST CASE X: UPDATE POSTAL CODE FAILURE
     def test_update_postal_failure(self, *_):
-        self.open(base_url + '/updateBillingPostal')
+        self.open(base_url + 'baseUpdate/updateBillingPostal')
         self.type("#postalCode", "4k5 b7g")
         self.click('input[type="submit"]')
