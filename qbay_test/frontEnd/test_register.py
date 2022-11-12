e52f25545a35cc8a1d70e929d59050560d82915f
Anjali Patel <pa4904550@gmail.com>
2022-11-11 22:44:06 -0500

    add additional test cases using shotgun testing

--- qbay_test/frontEnd/test_register.py
+++ qbay_test/frontEnd/test_register.py
@@ -637,3 +637,451 @@ class FrontEndHomePageTest(BaseCase):
         self.click('input[type="submit"]')
         # test if the message is correct
         self.assert_text("The passwords do not match", "h4")
+
+    def test_register_shotgun_1(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "testshot1@test.com")
+        self.type("#name", "usershot1")
+        self.type("#password", "pA$s123")
+        self.type("#password2", "pA$s123")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+        # test if the message is correct
+        self.assert_text("Enter your email and password", "h4")
+
+    def test_register_shotgun_2(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        Here, name is 2 characters.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "testregshot2@test.com")
+        self.type("#name", "Re")
+        self.type("#password", "Rental$123")
+        self.type("#password2", "Rental$123")
+
+        # click enter button
+        self.click('input[type="submit"]')
+        # Test if the message is correct.
+        self.assert_text("Registration failed.", "h4")
+
+    def test_register_shotgun_3(self, *_):
+        """
+        This is a sample black box shotgun test case. Registration Fails.
+        Here, input is long email, but password2 does not match with password.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "testshotreallyreallylongstrquenessint3@test.com")
+        self.type("#name", "usershot3")
+        self.type("#password", "pA#s123")
+        self.type("#password2", "pA$s123")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+        # Test if the message is correct.
+        self.assert_text("The passwords do not match", "h4")
+ 
+    def test_register_shotgun_4(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        Here, just password does not meet requirements.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "testshot4@test.com")
+        self.type("#name", "usershot4")
+        self.type("#password", "prop")
+        self.type("#password2", "prop")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+        # Test if the message is correct.
+        self.assert_text("Registration failed.", "h4")
+ 
+    def test_register_shotgun_5(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        Here, name is incorrect because name begins with a space.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "testshotdance5@test.com")
+        self.type("#name", " junioruser5")
+        self.type("#password", "Pa#dance5")
+        self.type("#password2", "Pa#dance5")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+        # Test if the message is correct.
+        self.assert_text("Registration failed.", "h4")
+ 
+    def test_register_shotgun_6(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        Here, more than one special character is used in password.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "testshot6@test.com")
+        self.type("#name", "usershot6")
+        self.type("#password", "Pa!danc#$6")
+        self.type("#password2", "Pa!danc#$6")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+        # test if the message is correct
+        self.assert_text("Enter your email and password", "h4")
+ 
+    def test_register_shotgun_7(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        Here, username is in all capitals. However, there is
+        no lowercase character in the password.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "test.shot7@test.com")
+        self.type("#name", "USERSHOT7")
+        self.type("#password", "PA!USERS$7")
+        self.type("#password2", "PA!USERS$7")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+        # Test if the message is correct.
+        self.assert_text("Registration failed.", "h4")
+ 
+    def test_register_shotgun_8(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        Here, password has no character with a lowercase and
+        email does not have @ character.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "test-shot8test.com")
+        self.type("#name", "USERSHOT8")
+        self.type("#password", "YO?USERS#8")
+        self.type("#password2", "YO?USERS#8")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+        # Test if the message is correct.
+        self.assert_text("Registration failed.", "h4")
+   
+    def test_register_shotgun_9(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "testshot9@test.com")
+        self.type("#name", "usershot9")
+        self.type("#password", "Apuff%123")
+        self.type("#password2", "Apuff$s123")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+        # test if the message is correct
+        self.assert_text("The passwords do not match", "h4")
+ 
+    def test_register_shotgun_10(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "tests+hot10@test.com")
+        self.type("#name", "usershot10")
+        self.type("#password", "Pa$S123")
+        self.type("#password2", "Pa$S123")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+        # test if the message is correct
+        self.assert_text("Enter your email and password", "h4")
+
+    def test_register_shotgun_11(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        Here, password do not have any alphabet.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "testshot11@test.com")
+        self.type("#name", "usershot11")
+        self.type("#password", "$@$#$123")
+        self.type("#password2", "$@$#$123")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+        # test if the message is correct
+        self.assert_text("Registration failed.", "h4")
+ 
+    def test_register_shotgun_12(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "testshot12@test.com")
+        self.type("#name", "usershot12")
+        self.type("#password", "s@$#$123")
+        self.type("#password2", "$@$#$123")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+        # test if the message is correct
+        self.assert_text("The passwords do not match", "h4")
+   
+    def test_register_shotgun_13(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        Here, name is invalid.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "testshot13@test.com")
+        self.type("#name", "#5usershot!13")
+        self.type("#password", "pA$s123")
+        self.type("#password2", "pA$s123")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+        # Test if the message is correct.
+        self.assert_text("Registration failed.", "h4")
+ 
+    def test_register_shotgun_14(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        Here, email contains space, so registration fail.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "test - shot14@test.com")
+        self.type("#name", "USERSHOT14")
+        self.type("#password", "wOrd$123")
+        self.type("#password2", "wOrd$123")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+        # Test if the message is correct.
+        self.assert_text("Registration failed.", "h4")
+   
+    def test_register_shotgun_15(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        Here, email contains brackets and symbol, so registration fail.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "testshot(1+5)@test.com")
+        self.type("#name", "usershotg15")
+        self.type("#password", "wOrd$123")
+        self.type("#password2", "wOrd$123")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+ 
+        # Test if the message is correct.
+        self.assert_text("Registration failed.", "h4")
+   
+    def test_register_shotgun_16(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        Here, name has upper, lowecase and numebers; registration test pass.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "testshot15@test.com")
+        self.type("#name", "16Usershot")
+        self.type("#password", "576$Pell")
+        self.type("#password2", "576$Pell")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+        # test if the message is correct
+        self.assert_text("Enter your email and password", "h4")
+ 
+    def test_register_shotgun_17(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        Here, email has just 2 characters before @.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "be@test.com")
+        self.type("#name", "usershot17")
+        self.type("#password", "576$Pell")
+        self.type("#password2", "576$Pell")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+        # test if the message is correct
+        self.assert_text("Enter your email and password", "h4")
+   
+    def test_register_shotgun_18(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        Here, email has just numbers and special characters.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "+18-5@test.com")
+        self.type("#name", "usershot18")
+        self.type("#password", "5d76$Pell")
+        self.type("#password2", "5d76$Pell")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+        # test if the message is correct
+        self.assert_text("Enter your email and password", "h4")
+   
+    def test_register_shotgun_19(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "tests_7hot19@test.com")
+        self.type("#name", "userShot19")
+        self.type("#password", "Pa$S123")
+        self.type("#password2", "Pa$S123")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+        # test if the message is correct
+        self.assert_text("Enter your email and password", "h4")
+ 
+    def test_register_shotgun_20(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "testshot20@test.com")
+        self.type("#name", "usershotisreallyreallyreallyreallylong20")
+        self.type("#password", "Spell#123")
+        self.type("#password2", "Spell#123")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+ 
+        # Test if the message is correct.
+        self.assert_text("Registration failed.", "h4")
+   
+    def test_register_shotgun_21(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        Here, name is too long.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "testshot21@test.com")
+        self.type("#name", "usershotisreallyreallyreallyreallylong21")
+        self.type("#password", "SpellSpellSpellSpellSpellSpellSpellSpell#123")
+        self.type("#password2", "SpellSpellSpellSpellSpellSpellSpellSpell#123")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+        # test if the message is correct
+        self.assert_text("Registration failed.", "h4")
+ 
+    def test_register_shotgun_22(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        Here, email
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "test234143343143143431234123413shot21@test.com")
+        self.type("#name", "usershot22")
+        self.type("#password", "pA$s723")
+        self.type("#password2", "pA$s723")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+        # test if the message is correct
+        self.assert_text("Enter your email and password", "h4")
+   
+    def test_register_shotgun_23(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        Here, email has numbers in between.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "test21shot_94s@test.com")
+        self.type("#name", "usershoT23")
+        self.type("#password", "pA$ss823")
+        self.type("#password2", "pA$ss823")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+        # test if the message is correct
+        self.assert_text("Enter your email and password", "h4")
+ 
+    def test_register_shotgun_24(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        Here, name has non alphanumeric symbols.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "test24shotg@test.com")
+        self.type("#name", "user$$hoT23")
+        self.type("#password", "pA$ss824")
+        self.type("#password2", "pA$ss824")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+        # Test if the message is correct.
+        self.assert_text("Registration failed.", "h4")
+   
+    def test_register_shotgun_25(self, *_):
+        """
+        This is a sample black box shotgun test case.
+        Here, password2 does not match password.
+        """
+        # open register page
+        self.open(base_url + '/register')
+        # fill email, name, password and password2
+        self.type("#email", "test24shotg@test.com")
+        self.type("#name", "usershot25")
+        self.type("#password", "mI$ss825")
+        self.type("#password2", "mIsss825")
+ 
+        # click enter button
+        self.click('input[type="submit"]')
+        # Test if the message is correct.
+        self.assert_text("The passwords do not match", "h4")
